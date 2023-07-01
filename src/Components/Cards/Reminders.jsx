1f35e44d61af3aec97a83a5450f80af00d6d7040
Amos Kyalo <amoskyalo927@gmail.com>
2023-07-01 21:43:15 +0300

    edited readme

--- src/Components/Cards/Reminders.jsx
+++ src/Components/Cards/Reminders.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import DeaultButton from '../Buttons/Deault'
+import React from "react";
+import DeaultButton from "../Buttons/Deault";
 
 const Reminders = () => {
   return (
-    <div className='bg-white p-6 rounded-2xl w-full'>
-        <div className="flex flex-row items-center justify-between">
+    <div className="bg-white p-6 rounded-2xl w-full">
+      <div className="flex flex-row items-center justify-between">
         <h2 className="font-semibold text-lg">Reminder</h2>
-        <DeaultButton title="+ Add New" color="#A162F7"/>
-        </div>
+        <DeaultButton title="+ Add New" color="#A162F7" />
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Reminders
+export default Reminders;
+
